f9a700e0878783bf7629b58308ef73c7c5ccc4ea
unknown <zhafirafazilla0@gmail.com>
2026-03-16 13:58:51 +0700

    update nama

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -38,7 +38,7 @@ export default function HeroSection() {
             transition={{ duration: 0.8, delay: 0.3 }}
             className="font-display text-4xl md:text-6xl lg:text-7xl font-bold mb-6"
           >
-            Fullstack Developer
+            Projek Fira
             <br />
             <span className="text-gradient">&amp; Content Creator</span>
           </motion.h1>
